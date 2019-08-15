e326e961b5b0aed2611aaacdf0601c97fc02036a
kbhit <gurgul.grzegorz@gmail.com>
2019-08-15 09:42:37 +0200

    CoefficientMatricesOutputFormat

---
+++ src/main/java/edu.agh.iga.adi.giraph/direction/io/CoefficientMatricesOutputFormat.java
@@ -0,0 +1,33 @@
+package edu.agh.iga.adi.giraph.direction.io;
+
+import edu.agh.iga.adi.giraph.direction.io.data.IgaElementWritable;
+import edu.agh.iga.adi.giraph.direction.io.data.IgaOperationWritable;
+import org.apache.giraph.graph.Vertex;
+import org.apache.giraph.io.formats.TextVertexOutputFormat;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+import java.io.IOException;
+
+import static org.apache.commons.lang3.StringUtils.join;
+
+public final class CoefficientMatricesOutputFormat extends TextVertexOutputFormat<LongWritable, IgaElementWritable, IgaOperationWritable> {
+
+  @Override
+  public TextVertexWriter createVertexWriter(TaskAttemptContext context) {
+    return new IdWithValueVertexWriter();
+  }
+
+  protected class IdWithValueVertexWriter extends TextVertexWriterToEachLine {
+
+    @Override
+    protected Text convertVertexToLine(Vertex<LongWritable, IgaElementWritable, IgaOperationWritable> vertex) throws IOException {
+      return new Text(
+          vertex.getId().get() + " " + join(vertex.getValue().getElement().mx.data, ",")
+      );
+    }
+
+  }
+
+}
--- src/test/java/edu/agh/iga/adi/giraph/GraphIT.java
+++ src/test/java/edu/agh/iga/adi/giraph/GraphIT.java
@@ -2,9 +2,9 @@ package edu.agh.iga.adi.giraph;
 
 import edu.agh.iga.adi.giraph.direction.DirectionComputation;
 import edu.agh.iga.adi.giraph.direction.IgaAdiWorkerContext;
+import edu.agh.iga.adi.giraph.direction.io.CoefficientMatricesOutputFormat;
 import edu.agh.iga.adi.giraph.direction.io.InMemoryStepInputFormat;
 import edu.agh.iga.adi.giraph.test.GiraphTestJob;
-import org.apache.giraph.io.formats.InMemoryVertexOutputFormat;
 import org.junit.jupiter.api.Test;
 
 import static edu.agh.iga.adi.giraph.IgaConfiguration.HEIGHT_PARTITIONS;
@@ -19,7 +19,7 @@ class GraphIT {
         .computationClazz(DirectionComputation.class)
         .workerContextClazz(IgaAdiWorkerContext.class)
         .vertexInputFormatClazz(InMemoryStepInputFormat.class)
-        .vertexOutputFormatClazz(InMemoryVertexOutputFormat.class)
+        .vertexOutputFormatClazz(CoefficientMatricesOutputFormat.class)
         .configuration(conf -> {
           PROBLEM_SIZE.set(conf, 12);
           HEIGHT_PARTITIONS.set(conf, 2);
