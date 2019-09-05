95345b804a4c667b8289f07209a5b799e6fc03ea
kbhit <gurgul.grzegorz@gmail.com>
2019-09-05 21:08:18 +0200

    debugging 1

--- modules/core/src/main/java/edu/agh/iga/adi/giraph/core/setup/CoefficientSolution.java
+++ modules/core/src/main/java/edu/agh/iga/adi/giraph/core/setup/CoefficientSolution.java
@@ -5,6 +5,7 @@ import edu.agh.iga.adi.giraph.core.problem.PartialSolution;
 import edu.agh.iga.adi.giraph.core.splines.BSpline1;
 import edu.agh.iga.adi.giraph.core.splines.BSpline2;
 import edu.agh.iga.adi.giraph.core.splines.BSpline3;
+import lombok.val;
 import org.ojalgo.structure.Access2D;
 
 public final class CoefficientSolution implements PartialSolution {
@@ -14,28 +15,36 @@ public final class CoefficientSolution implements PartialSolution {
   private static final BSpline3 b3 = new BSpline3();
 
   private final Mesh mesh;
-  private final Access2D<Double> coefficients;
+  private final Access2D<Double> coef;
 
   public CoefficientSolution(Mesh mesh, Access2D<Double> coefficients) {
     this.mesh = mesh;
-    this.coefficients = coefficients;
+    this.coef = coefficients;
   }
 
   @Override
   public double valueAt(double x, double y) {
-    final int ielemx = (int) (x / mesh.getDx()) + 1;
-    final int ielemy = (int) (y / mesh.getDy()) + 1;
-    final double localx = x - mesh.getDx() * (ielemx - 1);
-    final double localy = y - mesh.getDy() * (ielemy - 1);
-    return b1.getValue(localx) * b1.getValue(localy) * coefficients.doubleValue(ielemx, ielemy)
-        + b1.getValue(localx) * b2.getValue(localy) * coefficients.doubleValue(ielemx, ielemy + 1)
-        + b1.getValue(localx) * b3.getValue(localy) * coefficients.doubleValue(ielemx, ielemy + 2)
-        + b2.getValue(localx) * b1.getValue(localy) * coefficients.doubleValue(ielemx + 1, ielemy)
-        + b2.getValue(localx) * b2.getValue(localy) * coefficients.doubleValue(ielemx + 1, ielemy + 1)
-        + b2.getValue(localx) * b3.getValue(localy) * coefficients.doubleValue(ielemx + 1, ielemy + 2)
-        + b3.getValue(localx) * b1.getValue(localy) * coefficients.doubleValue(ielemx + 2, ielemy)
-        + b3.getValue(localx) * b2.getValue(localy) * coefficients.doubleValue(ielemx + 2, ielemy + 1)
-        + b3.getValue(localx) * b3.getValue(localy) * coefficients.doubleValue(ielemx + 2, ielemy + 2);
+    val ielemx = (long) (x / mesh.getDx());
+    val ielemy = (long) (y / mesh.getDy());
+    val localx = x - mesh.getDx() * ielemx;
+    val localy = y - mesh.getDy() * ielemy;
+
+    val sp1x = b1.getValue(localx);
+    val sp1y = b1.getValue(localy);
+    val sp2y = b2.getValue(localy);
+    val sp2x = b2.getValue(localx);
+    val sp3y = b3.getValue(localy);
+    val sp3x = b3.getValue(localx);
+
+    return coef.doubleValue(0, ielemy) * sp1x * sp1y +
+        coef.doubleValue(0, ielemy + 1) * sp1x * sp2y +
+        coef.doubleValue(0, ielemy + 2) * sp1x * sp3y +
+        coef.doubleValue(1, ielemy) * sp2x * sp1y +
+        coef.doubleValue(1, ielemy + 1) * sp2x * sp2y +
+        coef.doubleValue(1, ielemy + 2) * sp2x * sp3y +
+        coef.doubleValue(2, ielemy) * sp3x * sp1y +
+        coef.doubleValue(2, ielemy + 1) * sp3x * sp2y +
+        coef.doubleValue(2, ielemy + 2) * sp3x * sp3y;
   }
 
 }
