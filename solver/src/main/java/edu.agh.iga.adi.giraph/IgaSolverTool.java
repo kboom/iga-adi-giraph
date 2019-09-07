baf1b0a166d5dcaaa01da373248b5699280c8456
kbhit <gurgul.grzegorz@gmail.com>
2019-09-07 15:12:59 +0200

    Working running from maven

--- solver/src/main/java/edu.agh.iga.adi.giraph/IgaSolverTool.java
+++ solver/src/main/java/edu.agh.iga.adi.giraph/IgaSolverTool.java
@@ -1,13 +1,17 @@
 package edu.agh.iga.adi.giraph;
 
+import lombok.val;
 import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
 
 import static edu.agh.iga.adi.giraph.direction.IgaGiraphJobFactory.igaJob;
 
 public class IgaSolverTool implements Tool {
+
+  private static final Logger LOG = Logger.getLogger(IgaSolverTool.class);
 
   private Configuration config;
 
@@ -17,10 +21,13 @@ public class IgaSolverTool implements Tool {
 
   @Override
   public int run(String[] strings) {
+    LOG.info("Running computations");
+    val job = igaJob(giraphConfiguration());
     try {
-      igaJob(giraphConfiguration()).run(true);
+      job.run(true);
       return 0;
     } catch (Exception e) {
+      LOG.error("Could not run computations", e);
       return -1;
     }
   }
