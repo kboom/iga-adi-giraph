5163de4c7d33751edcbf1df41d65d5e820b1c36a
kbhit <gurgul.grzegorz@gmail.com>
2019-09-07 15:46:26 +0200

    Recognizing right input format

--- modules/direction/src/main/java/edu/agh/iga/adi/giraph/direction/IgaGiraphJobFactory.java
+++ modules/direction/src/main/java/edu/agh/iga/adi/giraph/direction/IgaGiraphJobFactory.java
@@ -1,6 +1,8 @@
 package edu.agh.iga.adi.giraph.direction;
 
+import com.google.common.collect.ImmutableMap;
 import edu.agh.iga.adi.giraph.direction.io.IgaEdgeInputFormat;
+import edu.agh.iga.adi.giraph.direction.io.InMemoryStepInputFormat;
 import edu.agh.iga.adi.giraph.direction.io.StepVertexInputFormat;
 import edu.agh.iga.adi.giraph.direction.io.StepVertexOutputFormat;
 import edu.agh.iga.adi.giraph.direction.io.data.IgaElementWritable;
@@ -8,17 +10,27 @@ import edu.agh.iga.adi.giraph.direction.io.data.IgaMessageWritable;
 import edu.agh.iga.adi.giraph.direction.io.data.IgaOperationWritable;
 import lombok.NoArgsConstructor;
 import org.apache.giraph.conf.GiraphConfiguration;
+import org.apache.giraph.io.VertexInputFormat;
 import org.apache.giraph.job.GiraphJob;
 import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
+import java.util.Map;
 
+import static edu.agh.iga.adi.giraph.direction.IgaConfiguration.INITIALISATION_TYPE;
+import static edu.agh.iga.adi.giraph.direction.computation.IgaComputationResolvers.COEFFICIENTS_PROBLEM;
+import static edu.agh.iga.adi.giraph.direction.computation.IgaComputationResolvers.SURFACE_PROBLEM;
 import static java.lang.Integer.MAX_VALUE;
 import static lombok.AccessLevel.PRIVATE;
 import static org.apache.giraph.conf.GiraphConstants.*;
 
 @NoArgsConstructor(access = PRIVATE)
 public class IgaGiraphJobFactory {
+
+  private static final Map<String, Class<? extends VertexInputFormat>> inputFormatsByInitType = ImmutableMap.of(
+      SURFACE_PROBLEM.getType(), InMemoryStepInputFormat.class,
+      COEFFICIENTS_PROBLEM.getType(), StepVertexInputFormat.class
+  );
 
   public static GiraphJob igaJob(GiraphConfiguration configuration) {
     return doCreateJob(injectSolverConfiguration(configuration));
@@ -28,7 +40,7 @@ public class IgaGiraphJobFactory {
     conf.setMasterComputeClass(StepComputation.class);
     conf.setWorkerContextClass(IgaWorkerContext.class);
     conf.setEdgeInputFormatClass(IgaEdgeInputFormat.class);
-    conf.setVertexInputFormatClass(StepVertexInputFormat.class);
+    conf.setVertexInputFormatClass(inputFormatsByInitType.get(INITIALISATION_TYPE.get(conf)));
     conf.setVertexOutputFormatClass(StepVertexOutputFormat.class);
     conf.setGraphPartitionerFactoryClass(IgaPartitionerFactory.class);
     VERTEX_ID_CLASS.set(conf, LongWritable.class);
