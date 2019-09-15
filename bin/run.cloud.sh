39e6fce128491bc5a61822c4624538bd0d49e06c
kbhit <gurgul.grzegorz@gmail.com>
2019-09-15 14:30:53 +0200

    more scripts

--- bin/run.cloud.sh
+++ bin/run.cloud.sh
@@ -3,11 +3,13 @@ THIS_DIR=`dirname "$THIS"`
 IGA_HOME=`cd "$THIS_DIR/.." ; pwd`
 
 TIMESTAMP=$(date +%s)
+HDFS_RESULTS_DIR=hdfs://iga-adi-m/user/kbhit/${TIMESTAMP}
 
 # This makes sure our fat JAR including Giraph classes is available for master and for looking for JAR file to send to HDFS for workers
 export HADOOP_CLASSPATH=$(pwd)/*
 
 set +x;
+export HDFS_RESULTS_DIR=${HDFS_RESULTS_DIR}
 yarn jar solver-1.0-SNAPSHOT.jar edu.agh.iga.adi.giraph.IgaSolverTool \
-	-o hdfs://iga-adi-m/user/kbhit/${TIMESTAMP} \
+	-o ${HDFS_RESULTS_DIR} \
 	"$@"
