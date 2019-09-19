c39aacb2187fbdd74399a243db8d198769f3436f
kbhit <gurgul.grzegorz@gmail.com>
2019-09-19 22:28:58 +0200

    Fixes aggregations at the worker context

--- modules/direction/src/main/java/edu/agh/iga/adi/giraph/direction/IgaWorkerContext.java
+++ modules/direction/src/main/java/edu/agh/iga/adi/giraph/direction/IgaWorkerContext.java
@@ -1,21 +1,26 @@
 package edu.agh.iga.adi.giraph.direction;
 
 import edu.agh.iga.adi.giraph.direction.io.StepVertexOutputFormat;
-import lombok.val;
 import org.apache.giraph.worker.DefaultWorkerContext;
+import org.apache.hadoop.io.BooleanWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.log4j.Logger;
 
-import static edu.agh.iga.adi.giraph.direction.Flags.INT_TRUE;
-import static edu.agh.iga.adi.giraph.direction.IgaCounter.ENDING_SUPER_STEP;
-import static edu.agh.iga.adi.giraph.direction.IgaCounter.STEP_COUNTER;
+import static edu.agh.iga.adi.giraph.direction.StepAggregators.ENDING_SUPER_STEP_OF_STEP;
+import static edu.agh.iga.adi.giraph.direction.StepAggregators.STEP;
+import static org.apache.log4j.Logger.getLogger;
 
 public final class IgaWorkerContext extends DefaultWorkerContext {
 
+  private static final Logger LOG = getLogger(IgaWorkerContext.class);
+
   @Override
   public void preSuperstep() {
-    val c = getContext().getCounter(ENDING_SUPER_STEP);
-    val s = getContext().getCounter(STEP_COUNTER);
-    StepVertexOutputFormat.step = (int) s.getValue();
-    StepVertexOutputFormat.isLast = c.getValue() == INT_TRUE;
+    BooleanWritable c = getAggregatedValue(ENDING_SUPER_STEP_OF_STEP);
+    IntWritable s = getAggregatedValue(STEP);
+    LOG.info("Ending super step " + c.get() + ", step " + s.get());
+    StepVertexOutputFormat.step = s.get();
+    StepVertexOutputFormat.isLast = c.get();
   }
 
 }
