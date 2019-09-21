23cbcf077ffd7fd4b3a6176f75a0c931855308ea
kbhit <gurgul.grzegorz@gmail.com>
2019-09-21 21:10:23 +0200

    Adds missing updates

--- bin/run.default.sh
+++ bin/run.default.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 exec ./run.cloud.sh \
   -s 2 \
-  -e 1536 \
+  -e 768 \
 	-w 4 \
 	-h 4 \
 	-t surface \
 	-p HEAT \
 	--init-problem RADIAL \
-	-c giraph.useInputSplitLocality=false \
 	-c giraph.yarn.task.heap.mb=2048 \
-  -c giraph.logLevel=error \
-	-c giraph.metrics.enable=true \
-	-c giraph.useNettyDirectMemory=true \
-	-c giraph.useNettyPooledAllocator=true
+  -c giraph.logLevel=error
