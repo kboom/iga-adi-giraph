69c93eb84cbcaf2ba81611727536e1adf62b3932
kbhit <gurgul.grzegorz@gmail.com>
2019-09-22 11:19:42 +0200

    Disables VERTEX_OUTPUT_FORMAT_THREAD_SAFE as it is not thread safe (at least not for now).

--- bin/run.standard.sh
+++ bin/run.standard.sh
@@ -2,15 +2,11 @@
 exec ./run.cloud.sh \
   -s 2 \
   -e 1536 \
-	-w 2 \
+	-w 4 \
 	-h 4 \
 	-t surface \
 	-p HEAT \
 	--init-problem RADIAL \
-	-c giraph.yarn.task.heap.mb=3000 \
-  -c giraph.numComputeThreads=2 \
-  -c giraph.numInputThreads=2 \
-  -c giraph.numOutputThreads=2 \
-  -c giraph.minPartitionsPerComputeThread=1 \
+	-c giraph.yarn.task.heap.mb=2048 \
   -c giraph.msgRequestWarningThreshold=1 \
   -c giraph.logLevel=error
