3c4a5954064b1d6f74c0b5a11810d54a09c0ae26
kbhit <gurgul.grzegorz@gmail.com>
2019-09-22 11:25:41 +0200

    Updates readme.

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-THIS_DIR=`dirname "$THIS"`
-BIN_DIR=`cd "$THIS_DIR/bin" ; pwd`
+THIS_DIR=$(dirname "$THIS")
+BIN_DIR=$(cd "$THIS_DIR/bin" || exit ; pwd)
 
 export PATH="${PATH}:${BIN_DIR}"
