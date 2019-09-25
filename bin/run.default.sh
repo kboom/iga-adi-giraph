5b8949326bc9452f9f05a824498334a517e9974d
kbhit <gurgul.grzegorz@gmail.com>
2019-09-25 18:26:27 +0200

    Stabilizes tests

--- bin/run.default.sh
+++ bin/run.default.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+export WORKER_JVM_OPTS="
+-Xmx4048M
+-Xms4048M
+-XX:+UseParNewGC
+-XX:NewSize=1024M
+-XX:MaxNewSize=1024M
+-XX:MaxTenuringThreshold=2
+-XX:SurvivorRatio=30
+-XX:-UseAdaptiveSizePolicy
+-XX:+UseConcMarkSweepGC
+-XX:+CMSParallelRemarkEnabled
+-XX:+ParallelRefProcEnabled
+-XX:+CMSClassUnloadingEnabled
+-XX:CMSInitiatingOccupancyFraction=80
+-XX:+UseCMSInitiatingOccupancyOnly
+"
+
 exec ./run.cloud.sh \
   -s 2 \
   -e 3092 \
@@ -14,4 +31,6 @@ exec ./run.cloud.sh \
   -c giraph.nettyRequestEncoderBufferSize=327680 \
   -c giraph.clientReceiveBufferSize=327680 \
   -c giraph.clientSendBufferSize=5242880 \
-  -c giraph.resendTimedOutRequests=false
+  -c giraph.resendTimedOutRequests=false \
+  -c yarn.app.mapreduce.am.command-opts="$WORKER_JVM_OPTS" \
+  -c yarn.app.mapreduce.am.resource.mb=2300
