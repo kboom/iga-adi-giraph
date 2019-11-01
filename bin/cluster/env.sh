365fa29e344f31125fe023084ba8e0b6b8304749
kbhit <gurgul.grzegorz@gmail.com>
2019-11-01 12:20:04 +0100

    Env file

--- bin/cluster/env.sh
+++ bin/cluster/env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export IGA_STEPS=2
 export IGA_PROBLEM_SIZE=1536
-export IGA_WORKERS=4
-export IGA_PARTITION_HEIGHT=4
-export IGA_WORKER_HEAP=2048
-export IGA_PERFTEST_NAME=perftest
+export IGA_WORKERS=1
+export IGA_WORKER_CORES=1
+export IGA_WORKER_MEMORY=1
+export IGA_PARTITION_HEIGHT=3
