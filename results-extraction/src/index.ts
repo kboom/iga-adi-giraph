a6c90bad5d9908b417de89d400420fad56de481d
kboom <gurgul.grzegorz@gmail.com>
2020-03-26 20:31:46 +0100

    test

--- results-extraction/src/index.ts
+++ results-extraction/src/index.ts
@@ -1,1 +1,11 @@
 export * from './lib/extract-suite';
+import { getopt } from 'stdio';
+import { extractSuite } from './lib/extract-suite';
+import { exportToExcel } from './lib/export-to-excel';
+
+const options = getopt({
+    'output': { key: 'o', required: true, description: 'The output excel file name' },
+    'input': { key: 'i', description: 'The input directory to scan through' }
+});
+
+exportToExcel(extractSuite(options.input as string), options.output as string)
