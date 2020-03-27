fa6d64cfc07c23cc2122c02f700590c6fa2b599b
kboom <gurgul.grzegorz@gmail.com>
2020-03-27 07:16:58 +0100

    working bin

--- results-extraction/src/index.ts
+++ results-extraction/src/index.ts
@@ -1,11 +1,2 @@
-export * from './lib/extract-suite';
-import { getopt } from 'stdio';
-import { extractSuite } from './lib/extract-suite';
-import { exportToExcel } from './lib/export-to-excel';
-
-const options = getopt({
-    'output': { key: 'o', required: true, description: 'The output excel file name' },
-    'input': { key: 'i', description: 'The input directory to scan through' }
-});
-
-exportToExcel(extractSuite(options.input as string), options.output as string)
+#!/usr/bin/env node
+import './load'
