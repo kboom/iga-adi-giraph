e30896b5e74f1248abc17e12a865255884170c60
kboom <gurgul.grzegorz@gmail.com>
2021-01-07 22:10:40 +0100

    print lscpu

--- bin/cluster/print.sys.info.sh
+++ bin/cluster/print.sys.info.sh
@@ -7,6 +7,7 @@ LOGS_DIR=${OUTPUT_DIR:-"$SCRIPT_PATH/logs"}
 mkdir -p "$LOGS_DIR"
 
 lscpu > "$LOGS_DIR/$RUN-lscpu.txt"
+lscpu --all --extended > "$LOGS_DIR/$RUN-cpuinfo-ext.txt"
 less /proc/cpuinfo > "$LOGS_DIR/$RUN-cpuinfo.txt"
 tail -n +1 /sys/devices/system/cpu/cpu*/topology/thread_siblings_list > "$LOGS_DIR/$RUN-topology-thread_siblings_list.txt"
 tail -n +1 /sys/devices/system/cpu/cpu*/topology/die_cpus_list > "$LOGS_DIR/$RUN-topology-die_cpus_list.txt"
